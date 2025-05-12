0d1de6eeaa9dfbaa74787507cda806c2c273677f
juane <juanesing222@gmail.com>
2025-05-11 19:42:30 -0500

    Terminación del CRUD para problemas y testcases

--- juez/urls.py
+++ juez/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import PruebaCodigo, CrearListarProblemas, ListaEnvios, CrearTestCase
+from .views import PruebaCodigo, CrearListarProblemas, ListaEnvios, CrearTestCase, EliminarProblema, ActualizarProblema, EliminarTestCase, ActualizarTestCase
+
 
 urlpatterns = [
-    path('problemas/', CrearListarProblemas.as_view(), name='problem-list-create'),
-    path('submissions/', ListaEnvios.as_view(), name='submission-list'),
-    path('testcases/', CrearTestCase.as_view(), name='create-testcase'),
-    path('run/', PruebaCodigo.as_view(), name='code-execute'),
+    path('problemas/', CrearListarProblemas.as_view(), name='listar-crear-problema'),
+    path('problemas/eliminar/<int:id>/', EliminarProblema.as_view(), name='eliminar-problema'),
+    path('problemas/editar/<int:id>/', ActualizarProblema.as_view(), name='ctualizar-problema'),
+    path('submissions/', ListaEnvios.as_view(), name='listar-submission'),
+    path('testcases/', CrearTestCase.as_view(), name='crear-testcase'),
+    path('testcases/eliminar/<int:id>/', EliminarTestCase.as_view(), name='eliminar-testcase'),
+    path('testcases/editar/<int:id>/', ActualizarTestCase.as_view(), name='actualizar-testcase'),
+    path('run/', PruebaCodigo.as_view(), name='ejecutor'),
 ]
